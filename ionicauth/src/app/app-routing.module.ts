6dad929b50a8c3d493ee1c64191509404aee9a40
Ghita Ghiti <ghitaghiti10@gmail.com>
2023-12-13 09:42:46 +0100

    install firebase version 9.16.0 , update project.

--- ionicauth/src/app/app-routing.module.ts
+++ ionicauth/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
-    path: 'signup',
+    path: 'signUp',
     loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
   },
   {
